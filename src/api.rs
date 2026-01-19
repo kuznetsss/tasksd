b73e0b4ab6c6d115d77c0a5ad3bd7bed8e0d29b5
Sergey Kuznetsov <kuzzz99@gmail.com>
2026-01-19 21:33:08 +0000

    Start from scratch

--- src/api.rs
+++
@@ -1,2 +0,0 @@
-pub mod client_messages;
-pub mod server_messages;
--- src/types.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct MessageID(pub usize);
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct ProcessID(pub usize);
