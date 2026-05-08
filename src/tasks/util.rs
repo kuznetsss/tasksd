b5d835f14c8748a0496ba4e7204fc307472b720b
Sergey Kuznetsov <kuzzz99@gmail.com>
2026-05-08 14:57:53 +0100

    Implemented TaskTrackerWrapper

--- src/tasks/util.rs
+++ src/tasks/util.rs
@@ -1,18 +1,64 @@
-use std::sync::Mutex;
+use std::{any::Any, panic::AssertUnwindSafe};
 
-use tokio::task::{AbortHandle, JoinSet};
+use futures::FutureExt;
+use tokio::task::AbortHandle;
 use tokio_util::task::TaskTracker;
+use tracing::error;
 
 use crate::tasks::task_error::TaskError;
 
-pub(in crate::tasks) struct WrappedTaskTracker {
+#[derive(Debug)]
+pub(in crate::tasks) struct WrappedTaskTracker<C = fn(Panic)> {
     inner: TaskTracker,
+    panic_handler: PanicHandler<C>,
 }
 
-impl WrappedTaskTracker {
-    pub(in crate::tasks) fn new() -> Self {
+#[derive(Debug, Clone)]
+pub(in crate::tasks) enum PanicHandler<C> {
+    Logging,
+    Callback(C),
+    Abort,
+}
+
+pub(in crate::tasks) type Panic = Box<dyn Any + Send + 'static>;
+
+impl<C> PanicHandler<C>
+where
+    C: FnOnce(Panic) + Clone + Send + 'static,
+{
+    fn handle(self, p: Panic) {
+        match self {
+            Self::Logging => {
+                error!("Panic: {}", Self::to_string(&p))
+            }
+            Self::Callback(c) => {
+                c(p);
+            }
+            Self::Abort => {
+                let log_msg = format!("Abort on panic: {}", Self::to_string(&p));
+                error!("{log_msg}");
+                eprintln!("{log_msg}");
+                std::process::abort();
+            }
+        }
+    }
+
+    fn to_string(p: &Panic) -> String {
+        p.downcast_ref::<&str>()
+            .map(|&s| s.to_string())
+            .or_else(|| p.downcast_ref::<String>().map(Clone::clone))
+            .unwrap_or("<non-string panic payload>".to_string())
+    }
+}
+
+impl<C> WrappedTaskTracker<C>
+where
+    C: FnOnce(Panic) + Clone + Send + 'static,
+{
+    pub(in crate::tasks) fn new(panic_handler: PanicHandler<C>) -> Self {
         Self {
             inner: TaskTracker::new(),
+            panic_handler,
         }
     }
 
@@ -20,10 +66,21 @@ impl WrappedTaskTracker {
     where
         F: Future<Output = ()> + Send + 'static,
     {
+        // TODO: there is a data race here: inner could get closed after the check and then spawn will create a detached task
         if self.inner.is_closed() {
             Err(TaskError::AlreadyExited)
         } else {
-            Ok(self.inner.spawn(f).abort_handle())
+            Ok(self
+                .inner
+                .spawn({
+                    let panic_handler = self.panic_handler.clone();
+                    async move {
+                        if let Err(panic) = AssertUnwindSafe(f).catch_unwind().await {
+                            panic_handler.handle(panic);
+                        }
+                    }
+                })
+                .abort_handle())
         }
     }
 
@@ -36,6 +93,7 @@ impl WrappedTaskTracker {
     }
 }
 
+/*
 #[cfg(test)]
 mod tests {
     use std::sync::{Arc, atomic::AtomicBool};
@@ -57,3 +115,4 @@ mod tests {
         assert!(called.load(std::sync::atomic::Ordering::Relaxed));
     }
 }
+*/
