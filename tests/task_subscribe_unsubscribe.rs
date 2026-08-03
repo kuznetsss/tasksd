60b2b55de3f20cc937ce3d8d274de4b686195a77
Sergey Kuznetsov <kuzzz99@gmail.com>
2026-08-03 23:37:03 +0100

    test: Fix flaky test (#45)

--- tests/task_subscribe_unsubscribe.rs
+++ tests/task_subscribe_unsubscribe.rs
@@ -1,8 +1,5 @@
 mod common;
 
-use std::time::Duration;
-
-use rustix::path::Arg;
 use tasksd::tasks::CHANNEL_CAPACITY;
 
 use crate::common::{
@@ -60,58 +57,28 @@ async fn unsubscribe_unsubscribes_from_output() {
     ctx.shutdown().await;
 }
 
-async fn wait_for_line_in_file(line: &str, file_path: &std::path::Path) {
-    const MAX_RETRIES: usize = 2000;
-    let mut it = 0;
-
-    loop {
-        let buf = std::fs::read_to_string(file_path).unwrap();
-        if buf.ends_with(line) {
-            break;
-        }
-        it += 1;
-        assert!(it < MAX_RETRIES);
-        tokio::time::sleep(Duration::from_millis(5)).await;
-    }
-}
-
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn subscribe_subscribes_to_output() {
-    let tmp_file = tempfile::NamedTempFile::new().unwrap();
-    let tmp_file_path = tmp_file.path().as_str().unwrap().to_string();
-
     let (ctx, mut client) = running_app().await;
 
-    client
-        .task_start("sh", &["-c", &format!("cat | tee {tmp_file_path}")], false)
-        .await
-        .unwrap();
+    client.task_start("cat", &[], false).await.unwrap();
 
     let response: TaskStartResponse = client.read_struct().await.unwrap();
     assert_eq!(response.id, client.last_id());
     let task_id = response.result.task_id;
 
-    let input = "input 1\n";
-    client.send_input(task_id, input).await.unwrap();
-
-    let response: TaskSendInputResponse = client.read_struct().await.unwrap();
-    assert_eq!(response.id, client.last_id());
-
-    wait_for_line_in_file(input, tmp_file.path()).await;
-
     client.subscribe(task_id).await.unwrap();
-
     let response: TaskSubscribeResponse = client.read_struct().await.unwrap();
     assert_eq!(response.id, client.last_id());
 
-    let input = "input 2\n";
+    let input = "input 1\n";
     client.send_input(task_id, input).await.unwrap();
     let last_id = client.last_id();
     client
         .expect_unordered()
         .message(move |r: TaskSendInputResponse| r.id == last_id)
         .message(move |o: TaskOutputNotification| {
-            o.params.task_id == task_id && o.params.line == input && o.params.line_number == 1
+            o.params.task_id == task_id && o.params.line == input && o.params.line_number == 0
         })
         .check()
         .await
@@ -133,33 +100,31 @@ async fn subscribe_subscribes_to_output() {
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn subscribe_to_already_running_task() {
-    let tmp_file = tempfile::NamedTempFile::new().unwrap();
-    let tmp_file_path = tmp_file.path().as_str().unwrap().to_string();
-
     let (ctx, mut client) = running_app().await;
 
-    client
-        .task_start("sh", &["-c", &format!("cat | tee {tmp_file_path}")], false)
-        .await
-        .unwrap();
-
+    client.task_start("cat", &[], true).await.unwrap();
     let response: TaskStartResponse = client.read_struct().await.unwrap();
     assert_eq!(response.id, client.last_id());
     let task_id = response.result.task_id;
 
     let input = "input 1\n";
     client.send_input(task_id, input).await.unwrap();
+    let last_id = client.last_id();
+    client
+        .expect_unordered()
+        .message(move |r: TaskSendInputResponse| r.id == last_id)
+        .message(move |n: TaskOutputNotification| {
+            n.params.line == input && n.params.line_number == 0
+        })
+        .check()
+        .await
+        .unwrap();
 
-    let response: TaskSendInputResponse = client.read_struct().await.unwrap();
-    assert_eq!(response.id, client.last_id());
-
-    wait_for_line_in_file(input, tmp_file.path()).await;
     drop(client);
-
     let mut client = ctx.make_client().await;
 
     client.subscribe(task_id).await.unwrap();
-    let response: TaskSendInputResponse = client.read_struct().await.unwrap();
+    let response: TaskSubscribeResponse = client.read_struct().await.unwrap();
     assert_eq!(response.id, client.last_id());
 
     let input = "input 2\n";
